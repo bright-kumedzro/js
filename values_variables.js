8a14c054800088968bd7319cea6d2d22134bb10e
Bright Kumedzro <kumedzrobright@gmail.com>
2024-10-02 19:28:22 +0000

    learnt values and variables

---
+++ values_variables.js
@@ -0,0 +1,7 @@
+let anime = "One Piece"
+let first_name = "Oda"
+let last_name ="Sensei"
+
+console.log(`${first_name} ${last_name} is the writer of ${anime}.`)
+if (anime=="One Piece") 
+    console.log("Oda is the greatest writer")
