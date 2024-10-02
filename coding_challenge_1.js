04caace017d18314534e95ae2b141b56d59d1a74
Bright Kumedzro <kumedzrobright@gmail.com>
2024-10-02 21:12:56 +0000

    Coding challange

---
+++ coding_challenge_1.js
@@ -0,0 +1,11 @@
+/*
+Mark and John are trying to compare their ImageBitmap(Body Mass Index), which is calcualted using the formula:
+BMI = mass / height ** 2 = mass / (height * height) - Mass in kg and height in mass
+
+1. Store Marka and John's mass and height in a variables
+2. Calculate both their BMI
+3. Check the boolean state of "markHigherBMI" and output it
+
+TEST DATA 1: Mark weighs 78kg and is 1.69m tall. John weighs 92kg and is 1.95m tall
+TEST DATA 1: Mark weighs 95kg and is 1.88m tall. John weighs 85kg and is 1.76m tall
+*/
