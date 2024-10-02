33ca316215cb8e33b9cdcb53e9825116ff79b2d4
Bright Kumedzro <kumedzrobright@gmail.com>
2024-10-02 20:43:53 +0000

    let, const and var declarations

---
+++ let_const_var.js
@@ -0,0 +1,12 @@
+let name = "Oda";
+name = "Oda Sensei";
+console.log(name)
+
+//let name - Just declaring it throws no errors
+
+const pi = 3.142;
+console.log(pi)
+
+var anime = "One Piece";
+anime = "Attack on Titan";
+console.log(anime)
