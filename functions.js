d311e99133b2d37f7a304f52ecee2d3021c93416
Bright Kumedzro <kumedzrobright@gmail.com>
2024-10-03 03:22:36 +0000

    Functions

---
+++ functions.js
@@ -0,0 +1,9 @@
+
+function fruitProcessor(apples,oranges){
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+
+console.log(fruitProcessor(2,5));
