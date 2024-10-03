d0c791a25ea0ec28abac63f6d6d4070b4a875d08
Bright Kumedzro <kumedzrobright@gmail.com>
2024-10-03 09:14:49 +0000

    Use of functions in funtions

--- Fundamental 2/functions.js
+++ Fundamental 2/functions.js
@@ -22,13 +22,18 @@ const fruitProcessor2 = function(apples, oranges){
 
 // Arrow Function
 
-let birthYear = 2000;
-const calcAge = birthYear => 2037 - birthYear;
-const age = calcAge(1992);
-console.log(age)
+// let birthYear = 2000;
+// const calcAge = birthYear => 2037 - birthYear;
+// const age = calcAge(1992);
+// console.log(age)
+
+const calcAge = function(birthYear) {
+    return 2037 - birthYear;
+}
 
 const yearsUntilRetirement = (firstName,birthYear) => {
-    const age = 2037 - birthYear;
+    // const age = 2037 - birthYear;
+    const age = calcAge(birthYear);
     const retirementAge = 65 - age;
     return `${firstName} retires in ${retirementAge} years`;
 }
