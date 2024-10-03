41b0c1169df524c515667f626d1fb3db8476a04e
Bright Kumedzro <kumedzrobright@gmail.com>
2024-10-03 01:42:26 +0000

    Strict and Loose Equality Operators

---
+++ equality_operators.js
@@ -0,0 +1,26 @@
+
+
+// Both Strict and Loose operators will print to the console
+// const age = 18;
+
+// if (age === 18) {
+//     console.log('You are 18 years old(Strict).');
+// }
+
+// if (age ==18) {
+//     console.log('You are 18 years old(Loose).');
+// }
+
+
+// Only the Loose operator will get printed to the console
+const age = "18";
+
+if (age === 18) {
+    console.log('You are 18 years old(Strict).');
+}
+
+if (age ==18) {
+    console.log('You are 18 years old(Loose).');
+}
+
+
