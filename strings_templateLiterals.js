7e4bac5c42caa046915f7df5925a716024d74729
Bright Kumedzro <kumedzrobright@gmail.com>
2024-10-03 00:14:54 +0000

    Strings and Template Literals

---
+++ strings_templateLiterals.js
@@ -0,0 +1,12 @@
+name = "Oda Sensei";
+
+const passage = "I'm a fan of " + name + "'s" + " writing";
+
+console.log(passage);
+
+const anime = "One Piece";
+const fav_character = "Roronoa Zoro";
+
+const message = `I sometimes wonder how ${name} goes about ${anime}'s creation. It's incredible! Especially ${fav_character}'s character development`;
+
+console.log(message);
