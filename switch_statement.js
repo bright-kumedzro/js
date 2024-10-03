c2bd36af310b0dfefbd592a80969b7f6e55735df
Bright Kumedzro <kumedzrobright@gmail.com>
2024-10-03 02:29:03 +0000

    Switch Statement

---
+++ switch_statement.js
@@ -0,0 +1,30 @@
+
+const day = "Thursday";
+
+switch (day) {
+    case "Monday":
+        console.log("Three days ago was Monday");
+        break;
+    case "Tuesday":
+        console.log("Two days ago was Tuesday");
+        break;
+    case "Wednesday":
+        console.log("Yesterday was Wednesday");
+        break;
+    case "Thursday":
+        console.log("Today's Thursday");
+        break;
+    case "Friday":
+        console.log("Tomorrow's Friday");
+        break;
+    case "Saturday":
+        console.log("Two days from now will be Saturday");
+        break;
+    case "Sunday":
+        console.log("Three days from now will be Sunday");
+        break;
+        default:
+        console.log("Invalid day of the week");
+        break;
+
+}
